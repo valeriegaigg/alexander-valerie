7d87ed89c76c091da243bf2978552d0ec98e52ed
Valerie Gaigg <300102994@fvtc.edu>
2026-05-20 11:03:22 -0500

    add form

--- site.js
+++ site.js
@@ -1,38 +1,5 @@
-var main = document.getElementById('main')
-var envelope = document.getElementById('envelope')
-var flap = document.getElementById('flap')
-var flap2 = document.getElementById('flap2')
-var cover = document.getElementById('cover')
-var title = document.querySelector('h1');
-var prompt = document.querySelector('h3');
-
-function move() {
-    flap.classList.add('open');
-    cover.textContent = "";
-    
-    setTimeout(() => {
-        main.classList.add('slide');
-        flap.style.zIndex = 1;
-        flap2.style.zIndex = 1;
-        envelope.style.zIndex = 10;
-    }, 1000);
-
-    setTimeout(() => {
-        show();
-    }, 4000);
-    
+window.onbeforeunload = () => {
+  for(const form of document.getElementsByTagName('form')) {
+    form.reset();
+  }
 }
-
-
-
-function show(){
-    envelope.classList.add('fade');
-    cover.classList.add('fade');
-    flap.classList.replace('open', 'fade');
-    flap2.classList.add('fade'); 
-    main.classList.add('grow');
-    title.classList.add('fade');
-    prompt.classList.add('fade');
-
-}
-
